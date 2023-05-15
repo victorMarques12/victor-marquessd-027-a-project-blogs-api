bccad64f7490014e614ecfbf4748154163281b8a
victor marques <victormarques.ym@gmail.com>
2023-05-15 19:26:30 -0300

    req2

--- src/models/User.js
+++ src/models/User.js
@@ -6,10 +6,26 @@
 
 const userModel = (sequelize, DataTypes) => {
   const userTable = sequelize.define('User', {
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      unique: true,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING
+    },
 
   },{
     timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
